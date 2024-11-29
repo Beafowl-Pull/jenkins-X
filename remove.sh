be24e54b1f6f47500ecdad75c86acefe8b7fa8fd
Matheo Martin <matheo.martin@epitech.eu>
2024-11-29 23:44:36 +0900

    Fix

---
+++ remove.sh
@@ -0,0 +1,5 @@
+kubectl delete ns jx --force
+kubectl delete ns jx-git-operator --force
+kubectl delete ns jx-production --force
+kubectl delete ns jx-staging --force
+kubectl delete ns tekton-pipelines --force
